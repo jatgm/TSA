508ffed27e6d65fe9cce86a93fd7c7ba73ef9a89
jatgm <jatgmalt@protonmail.com>
2023-01-14 19:11:23 -0800

    fdsa

--- Characters/cat.gd
+++ Characters/cat.gd
@@ -9,10 +9,6 @@ extends CharacterBody2D
 @onready var sprite_2d = $Sprite2d
 @onready var state_machine = animation_tree.get("parameters/playback")
 
-func _ready():
-	state_machine.travel("walk")
-	$AnimationPlayer2.play("blinking")
-
 func _physics_process(_delta): # happens 60 times a sec, underscore can represent unused variable
 	
 	var input_direction = Vector2(
@@ -20,13 +16,17 @@ func _physics_process(_delta): # happens 60 times a sec, underscore can represen
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
 	
-	
 	velocity = input_direction.normalized() * move_speed
 
 	#built in function for characterbody new godot 4! :D
 	move_and_slide()
 	
 	update_animation_parameters(input_direction)
+	
+	if Input.is_action_pressed("up") or Input.is_action_pressed("down"):
+		$AnimationPlayer2.stop()
+	else:
+		$AnimationPlayer2.play("blinking")
 	
 func update_animation_parameters(move_input : Vector2):
 	if move_input != Vector2.ZERO:
@@ -37,9 +37,7 @@ func update_animation_parameters(move_input : Vector2):
 		state_machine.travel("walk")
 	else:
 		state_machine.travel("blinking")
-		
-func rotate(degree):
-	print(degree)
+
 	
 
 	
