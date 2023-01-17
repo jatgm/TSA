2c4022f841b0e5e071c9b8ace23990ba4060d071
jatgm <jatgmalt@protonmail.com>
2023-01-16 20:07:39 -0800

    save f 4 niw

--- Characters/Sprite2d.gd
+++ Characters/Sprite2d.gd
@@ -1,7 +1,10 @@
 extends Sprite2D
 
+@onready var occluder = $LightOccluder2d
 
 # Called when the node enters the scene tree for the first time.
 func addrotation(degree : int):
+	print(rotation)
+	print(occluder.rotation)
+	occluder.rotation += degree
 	rotation += degree
-
