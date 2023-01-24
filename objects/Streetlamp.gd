0e8253391a578078fe2a07f66510d0324ce49cc9
jatgm <jatgmalt@protonmail.com>
2023-01-23 22:32:54 -0800

    yay

--- objects/Streetlamp.gd
+++ objects/Streetlamp.gd
@@ -5,10 +5,6 @@ extends Sprite2D
 enum LIGHT_STATE {ON,OFF}
 
 var state = LIGHT_STATE.ON
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
 	
 func light_flicker():
 	state = LIGHT_STATE.ON
@@ -17,12 +13,8 @@ func light_flicker():
 		light.energy = randf_range(0, 0.35)
 		await get_tree().create_timer(randf_range(0,1)).timeout
 		light.energy = 0.35
-	print("fart")
+
 	light.energy = 0
 
 func light_off():
 	state = LIGHT_STATE.OFF
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
