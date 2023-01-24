3b3e3066ad004efeebef3f547ac27c74e3a8dc10
jatgm <jatgmalt@protonmail.com>
2023-01-23 22:32:06 -0800

    yay!

--- objects/Streetlamp.gd
+++ objects/Streetlamp.gd
@@ -2,14 +2,26 @@ extends Sprite2D
 
 @onready var light = $PointLight2d
 
+enum LIGHT_STATE {ON,OFF}
+
+var state = LIGHT_STATE.ON
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	while true:
+	pass
+	
+func light_flicker():
+	state = LIGHT_STATE.ON
+	while state == LIGHT_STATE.ON:
 		await get_tree().create_timer(randf_range(0,2)).timeout
 		light.energy = randf_range(0, 0.35)
 		await get_tree().create_timer(randf_range(0,1)).timeout
 		light.energy = 0.35
+	print("fart")
+	light.energy = 0
 
+func light_off():
+	state = LIGHT_STATE.OFF
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
