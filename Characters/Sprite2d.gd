875db3d1aae3b0bc2c26e39e7595b50110bfba47
jatgm <jatgmalt@protonmail.com>
2023-01-31 21:48:33 -0800

    I AM FREEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- Characters/Sprite2d.gd
+++ Characters/Sprite2d.gd
@@ -4,7 +4,5 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func addrotation(degree : int):
-	print(rotation)
-	print(occluder.rotation)
 	occluder.rotation += degree
 	rotation += degree
