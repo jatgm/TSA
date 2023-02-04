627ab1f7678b0abbe8be7ce780027a20c9890268
jatgm <jatgmalt@protonmail.com>
2023-02-04 15:27:15 -0800

    THIS IS SO COOL

--- Levels/PauseMenu.gd
+++ Levels/PauseMenu.gd
@@ -1,19 +1,38 @@
 extends CanvasLayer
 
+func _process(delta):
+	print(AudioServer.get_bus_volume_db(0))
+
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
-		if $VBoxContainer.visible == false:
-			$VBoxContainer.visible = true
+		if $Selector.visible == false:
+			$Settings.visible = false
+			$Selector.visible = true
 		else:
 			get_tree().paused = !get_tree().paused
 			visible = !visible
 
 func _on_settings_pressed():
-	$VBoxContainer.visible = false
+	$Settings.visible = !$Settings.visible
+	$Selector.visible = !$Selector.visible
 
 func _on_resume_pressed():
 	get_tree().paused = !get_tree().paused
 	visible = !visible
 
 func _on_quit_pressed():
-	pass # Replace with function body.
+	get_tree().quit()
+
+func _on_back_button_pressed():
+	$Settings.visible = false
+	$Selector.visible = true
+
+func _on_master_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(0, linear_to_db(value))
+
+
+func _on_sfx_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(2, linear_to_db(value))
+
+func _on_music_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(1, linear_to_db(value))
