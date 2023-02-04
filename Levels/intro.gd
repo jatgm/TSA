8e19417fef36594a1052230758ceae72ab604bd9
jatgm <jatgmalt@protonmail.com>
2023-02-03 19:36:39 -0800

    I FEEL KINDA FREEEEEEEEEEEEEEEEEE

--- Levels/intro.gd
+++ Levels/intro.gd
@@ -5,6 +5,3 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animation_player.play('Intro')
-
-func _process(_delta):
-	print(Engine.get_frames_per_second())
---
+++ Levels/tsa.gd
@@ -0,0 +1,8 @@
+extends Node
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+
+		get_tree().paused = !get_tree().paused
+		$CanvasLayer/ColorRect.visible = !$CanvasLayer/ColorRect.visible	
+#
