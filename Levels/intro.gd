06aa3d31a77cf69be20c1c9d6a0c61e9579f712b
jatgm <jatgmalt@protonmail.com>
2023-02-09 22:03:07 -0800

    4 now

--- Levels/intro.gd
+++ Levels/intro.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
 @onready var animation_player = $AnimationPlayer
+@onready var dialog_player = $DialogPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animation_player.play('Intro')
+	
+func _process(_delta):
+	if Input.is_action_just_pressed("interact"):
+		dialog_player.start_dialogue()
