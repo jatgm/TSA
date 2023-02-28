0f2ead59fade3d86b3d237ed5ec9a2b7109cd0d8
jatgm <jatgmalt@protonmail.com>
2023-02-27 21:42:21 -0800

    YAY

--- objects/phonebooth.gd
+++ objects/phonebooth.gd
@@ -2,21 +2,29 @@ extends Node2D
 
 enum PHONEBOOTH_STATE {USING, NOT_USING}
 
+@export var open = false
 var state = PHONEBOOTH_STATE.NOT_USING
 
 func _on_interaction_manager_enter_phonebooth(interacter):
-	if state == PHONEBOOTH_STATE.NOT_USING:
-		state = PHONEBOOTH_STATE.USING
-		var tween = get_tree().create_tween()
-		interacter.moving = false
-		tween.tween_property(interacter, "position", position, 0.3).set_trans(Tween.TRANS_SINE)
-		$AnimationPlayer.play("add_blackbars")
+	if open:
+		if state == PHONEBOOTH_STATE.NOT_USING:
+			state = PHONEBOOTH_STATE.USING
+			var tween = get_tree().create_tween()
+			interacter.moving = false
+			tween.tween_property(interacter, "position", position, 0.3).set_trans(Tween.TRANS_SINE)
+			$AnimationPlayer.play("add_blackbars")
+			await get_tree().create_timer(0.4).timeout
+			$UI.visible = true
+		else:
+			state = PHONEBOOTH_STATE.NOT_USING
+			var tween = get_tree().create_tween()
+			$AnimationPlayer.play("remove_blackbars")
+			tween.tween_property(interacter, "position", position+Vector2(0,40), 0.3).set_trans(Tween.TRANS_SINE)
+			$UI.visible = false
+			await get_tree().create_timer(0.4).timeout
+			interacter.moving = true
 	else:
-		state = PHONEBOOTH_STATE.NOT_USING
-		var tween = get_tree().create_tween()
-		$AnimationPlayer.play("remove_blackbars")
-		tween.tween_property(interacter, "position", position+Vector2(0,40), 0.3).set_trans(Tween.TRANS_SINE)
-		await get_tree().create_timer(0.3).timeout
-		interacter.moving = true
+		$InteractionManager/DialogPlayer.start_dialogue()
+		
 		
 
