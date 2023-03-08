31c97dc3b214fe29eaed67bf0298eb98d66a0ae2
jatgm <jatgmalt@protonmail.com>
2023-03-08 15:48:39 -0800

    asdlkasd;fljasdlfjskadlf

--- Hitboxes/Interactions/fusebox_interaction.gd
+++ Hitboxes/Interactions/fusebox_interaction.gd
@@ -1,7 +1,22 @@
 extends InteractionManager
 
 func recieve_interaction(interacter) -> void:
-	$DialogPlayer.start_dialogue()
-	if started:
-		started = false
-		$AudioStreamPlayer.playing = true
+	if interacter.step == 4:
+		if $CanvasLayer.visible == true:
+			$CanvasLayer.visible = false
+			started = false
+			return
+		$CanvasLayer.visible = true
+		if started:
+			started = false
+			$AudioStreamPlayer.playing = true
+
+func hover() -> void:
+	$"Fuseboxtest-1png".material.set_shader_parameter("opacity", 0.4)
+	
+func unhover() -> void:
+	$"Fuseboxtest-1png".material.set_shader_parameter("opacity", 0)
+
+func exit_bound() -> void: # These two can be overriden 
+	$CanvasLayer.visible = false
+	started = true
